219d7e1a54430cbdc28d016a6d8f0e2b9dccc21e
A.Sharifi <alizadasharifi2022@gmail.com>
2025-03-10 21:35:45 -0500

    feat: setup application tailwind configration

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,14 @@
+import { Mountain } from "../components/icons";
+
 function Notfound() {
-  return <div>Notfound</div>;
+  return (
+    <div className="container flex flex-col items-center justify-center grow">
+     <Mountain className="bg-blue-200" />
+      <p className="text-black  text-base md:text-xl ">
+        صفحه مورد نظر شما یافت نشد!
+      </p>
+    </div>
+  );
 }
 
 export default Notfound;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,64 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        iranSans: ["iranSans", "sans-serif"],
+        vazir: ["vazir", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#417F56",
+          100: "#B7D9D6",
+          200: "#9FCDC9",
+          300: "#87C1BB",
+          400: "#6FB4AD",
+          500: "#57A7A0",
+          600: "#3F9B92",
+          700: "#278F85",
+          800: "#396F4B",
+          900: "#315F41",
+          1000: "#294F36",
+          1100: "#21402B",
+          1200: "#183020",
+          1300: "#102016",
+        },
+        neutral: {
+          100: "#FAFAFA",
+          200: "#F6F6F6",
+          300: "#EFEFEF",
+          400: "#E1E1E1",
+          500: "#D9D9D9",
+          600: "#CBCBCB",
+          700: "#ADADAD",
+          800: "#909090",
+          900: "#717171",
+          1000: "#505050",
+          1100: "#353535",
+          1200: "#212121",
+          1300: "#121212",
+        },
+        error: {
+          light: "#ED2E2E",
+          extralight: "#FFF2F2",
+        },
+        success: {
+          DEFAULT: "#00966D",
+          light: "#00BA88",
+          extralight: "#F3FDFA",
+        },
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+        },
+      },
+    },
   },
   plugins: [],
 };
